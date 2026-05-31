244d1909372d31da476388329de8d48c49cc7d52
Luca Schiavoi <jerv.norsk@gmail.com>
2026-05-31 17:00:28 +0200

    feat: add shadcn/ui package
    - Initial configuration of shadcn/ui to modernize UI

--- apps/portfolio/src/app/layout.tsx
+++ apps/portfolio/src/app/layout.tsx
@@ -1,6 +1,9 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono, JetBrains_Mono} from "next/font/google";
 import "./globals.css";
+import {ThemeProvider} from "next-themes";
+
+const jetbrainsMono = JetBrains_Mono({subsets: ['latin'], variable: '--font-mono'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +28,19 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+      className={`${jetbrainsMono.variable} ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <body className="min-h-full flex flex-col">
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+    </body>
     </html>
   );
 }
